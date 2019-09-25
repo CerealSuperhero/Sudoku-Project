628a2e15ca0d5ed01d7dcbbb6c7d30608d3b1428
CerealSuperhero <44500418+CerealSuperhero@users.noreply.github.com>
2019-09-26 00:04:56 +0100

    removed /www from gitignore

---
+++ myApp/www/js/sudoku/sudokucontroller.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('sudoku')
+        .controller('sudokucontroller', control);
+
+    control.$inject = [
+       
+        ];
+    
+    function control(
+        
+    ) {
+        var vm = angular.extend(this, {
+            
+         });
+        
+         console.log("ALIVE");
+         
+
+      
+
+        // TODO: Error Handling
+          
+    }
+})();
---
+++ myApp/www/js/sudoku/sudokumodule.js
@@ -0,0 +1,13 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('sudoku', [])
+        .config(function($stateProvider) {
+            $stateProvider
+            
+            
+                
+            });
+				
+})();
