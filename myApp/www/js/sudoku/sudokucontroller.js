0f8dc6e7ef816e0433f5bb0ef53fa4ddba162396
CerealSuperhero <44500418+CerealSuperhero@users.noreply.github.com>
2019-11-10 22:39:25 +0000

    implimented grid click hand;le & began game import

--- myApp/www/js/sudoku/sudokucontroller.js
+++ myApp/www/js/sudoku/sudokucontroller.js
@@ -27,19 +27,50 @@
 
     function init() {
 
+        /* for (var i = 0; i<20;i++){
+            outPut[i]="top Left= "+ w[wcount] + ", " + h[hcount];
+            itCount();
+            console.log(outPut[i]);
+          } */
+
+        
+
+
+
         startCanvas()
 
     }
+
+    var emptysudoku=    "530070000600195000098000060800060003400803001700020006060000280000419005000080079";
+    var filledSudokku=  "534678912672195348198342567859761423426853791713924856961537284287419635345286179";
+
+    function itCount(){
+        if (wcount==9){
+          wcount=0;
+          hcount++;
+        }else{
+          wcount++;
+        }
+      }
     var canvas;
     var ctx;
 
-    var gridSquares = {
-        "Square1": { "gridNumber": 1, "points": [{ "x": 0, "y": 2 }, { "x": 100, "y": 2 }, { "x": 100, "y": 100 }, { "x": 0, "y": 100 }], "currentValue": 1, "CorrectValue": 9, "hilighted":false },
-        "Square2": { "gridNumber": 5, "points": [{ "x": 100, "y": 100 }, { "x": 200, "y": 100 }, { "x": 200, "y": 200 }, { "x": 100, "y": 200 }], "currentValue": 1, "CorrectValue": 9 ,"hilighted":false },
-    }
+    /* var gridSquares = [
+        {"gridNumber": 1, "points": [{ "x": 0, "y": 2 }, { "x": 100, "y": 2 }, { "x": 100, "y": 100 }, { "x": 0, "y": 100 }], "currentValue": 1, "CorrectValue": 9, "hilighted":false },
+        {"gridNumber": 5, "points": [{ "x": 100, "y": 100 }, { "x": 200, "y": 100 }, { "x": 200, "y": 200 }, { "x": 100, "y": 200 }], "currentValue": 1, "CorrectValue": 9 ,"hilighted":false },
+    ] */
+
+    var gridSquares = [
+        {"gridNumber": 1,"x":[0,100], "y":[0,100], "points": [{ "x": 0, "y": 0 }, { "x": 100, "y": 0 }, { "x": 100, "y": 100 }, { "x": 0, "y": 100 }], "currentValue": 1, "CorrectValue": 9, "hilighted":false },
+        {"gridNumber": 5,"x":[100,200], "y":[100,200], "points": [{ "x": 100, "y": 100 }, { "x": 200, "y": 100 }, { "x": 200, "y": 200 }, { "x": 100, "y": 200 }], "currentValue": 1, "CorrectValue": 9 ,"hilighted":false },
+    ]
 
     function hilightSquare(gridSquare) {
         var points = gridSquare.points;
+         points[0]= { x:gridSquare.x[0], "y": gridSquare.y[0] }
+         points[1]= { x:gridSquare.x[1], "y": gridSquare.y[0] }
+         points[2]= { x:gridSquare.x[1], "y": gridSquare.y[1] }
+         points[3]= { x:gridSquare.x[0], "y": gridSquare.y[1] }
         ctx.fillStyle = "skyblue";
         ctx.strokeStyle = "black";
         ctx.lineWidth=1;
@@ -178,29 +209,75 @@
         var canvas = document.getElementById("myCanvas");
         var offsetX = canvas.offsetLeft;
         var offsetY = canvas.offsetTop;
-
+        //get rid of this when move into new app
+        //consider adding  code for orizontal offset and vertical offset not ujst header bar
+        var topBar = document.getElementsByClassName("bar-stable bar bar-header");
+        var topBarHeight = topBar[0].offsetHeight;
         var mouseX = parseInt(e.clientX - offsetX);
-        var mouseY = parseInt(e.clientY - offsetY - 44);
+        var mouseY = parseInt(e.clientY - offsetY - topBarHeight);
 
         console.log("clicked!", "x:" + mouseX, "y:" + mouseY);
         
-        if (mouseX<300) {
-            hilightSquare(gridSquares.Square1);
+        /* if (mouseX<300) {
+            toggleHilight(gridSquares[0]);
             
-        } else{
-            clearSquare(gridSquares.Square1);
-            
+        } */
+        var clickedSquare=null;
+        clickedSquare=getSquare(mouseX,mouseY);
+        if (clickedSquare!=null) {
+            toggleHilight(clickedSquare);
         }
+        //toggleHilight(getSquare(mouseX,mouseY));
+
         draw()
         
 
     }
 
-    function draw(){
-        initCanvas()
+    function getSquare(x,y){
+        var clickedSquare=null;
         gridSquares.forEach(square => {
-            //if(square.) is hilighted draw hilight
+            var minX=square.x[0];
+            var maxX=square.x[1];
+            var minY=square.y[0];
+            var maxY=square.y[1];
+
+            if ((minX<x) && (x<maxX ) && (minY<y) && (y<maxY)) {
+                clickedSquare=square;
+                
+            }
         });
+        
+        if (clickedSquare!=null) {
+            return clickedSquare;
+        }else{
+            console.warn("couldn't find clicked square");
+            return null;
+        }
+        
+
+    }
+
+    function toggleHilight(square){
+        square.hilighted=!square.hilighted;
+    }
+
+    function draw(){
+
+        
+        
+        gridSquares.forEach(square => {
+            if (square.hilighted) {
+                hilightSquare(square);
+            }else{
+                clearSquare(square);
+            }
+        });
+
+        initCanvas();
+
+        
+
         //do numbers
     }
 
