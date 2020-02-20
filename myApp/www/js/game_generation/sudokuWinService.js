2126fbff999c28a55d418932745c4fed68928491
CerealSuperhero <44500418+CerealSuperhero@users.noreply.github.com>
2020-02-20 15:07:09 +0000

    finished win service
    can now check a string to see if complete sudoku is valid

--- myApp/www/js/game_generation/sudokuWinService.js
+++ myApp/www/js/game_generation/sudokuWinService.js
@@ -19,12 +19,23 @@ angular.module('sudokuWinService',[])
     var order=[1, 2, 3, 11, 12, 13, 21, 22, 23, 4, 5, 6, 14, 15, 16, 24, 25, 26, 7, 8, 9, 17, 18, 19, 27, 28, 29,
     31, 32, 33, 41, 42, 43, 51, 52, 53, 34, 35, 36, 44, 45, 46, 54, 55, 56, 37, 38, 39, 47, 48, 49, 57, 58, 59,
     61, 62, 63, 71, 72, 73, 81, 82, 83, 64, 65, 66, 74, 75, 76, 84, 85, 86, 67, 68, 69, 77, 78, 79, 87, 88, 89];
+    var imported=[{}];
+
+    
 
 
     this.makeArray = function(sudokuString){
-        imported=[{}]
-        if (sudokuString.length()==81) {
+        
+        if (sudokuString.length==81) {
             for (var i = 0; i < order.length; i++) {
+
+                var removeSelf=function(x){
+                    if(x==order[i]){
+                      return false;
+                    }
+                    return true;
+                    
+                }
 
 
                 thisGrid = grids.find(function (square) {
@@ -38,13 +49,95 @@ angular.module('sudokuWinService',[])
                 });
 
 
-                imported[i] = { "position": order[i], "value": sudokuString[i], "toTry": [1, 2, 3, 4, 5, 6, 7, 8, 9], "Grid": thisGrid, "HLine": thisHLine, "VLine": thisVLine };
+                imported[order[i]] = { "position": order[i], "value": sudokuString[i], "toTry": [1, 2, 3, 4, 5, 6, 7, 8, 9], "Grid": thisGrid.filter(removeSelf), "HLine": thisHLine.filter(removeSelf), "VLine": thisVLine.filter(removeSelf) };
                 
             }
         }
         //imported[i] = { "position": i, "value": 0, "toTry": [1, 2, 3, 4, 5, 6, 7, 8, 9], "Grid": thisGrid, "HLine": thisHLine, "VLine": thisVLine };
+        //console.log(imported);
     }
 
+    this.checkWin = function (inputString) {
+        this.makeArray(inputString);
+        //imported.shift();
+        //console.log("i", imported);
+        var valid = true;
+        imported.forEach(function (square) {
+            if (square.position != null) {
+
+
+                //console.log("square", square);
+                //if ((!validNumberCheck(imported, square.position, square.value))||(!square.toTry.includes(square.value))) {
+                    //console.log(square.toTry.includes(parseInt(square.value)));
+                    //console.log(square.value);
+                    
+                    
+                if ((!validNumberCheck(imported, square.position, square.value))||(!square.toTry.includes(parseInt(square.value)))) {
+                    valid = false;
+                }
+            }
+        });
+        console.log("valid?",valid);
+        
+        return valid;
+    }
+
+    function validNumberCheck(doku,position,checkNumber){
+        /* console.log("doku",doku);
+        console.log("p",position);
+        console.log("num",checkNumber);
+         */
+        
+        
+        //console.log("check", doku[position]);
+        
+        gridTest = doku[position].Grid.reduce(function (testBool = true, gridCell) {
+            if (testBool == true) {
+                if (doku[gridCell].value != checkNumber) {
+                    //console.log(doku[gridCell].value +"!="+ checkNumber);
+                    return true
+                } else {
+                    return false;
+                };
+            } else {
+                return false;
+            };
+    
+        }, true);
+        //console.log(gridTest);
+        HLineTest = doku[position].HLine.reduce(function (testBool = true, HLineCell) {
+            if (testBool == true) {
+                if (doku[HLineCell].value != checkNumber) {
+                    //console.log(doku[HLineCell].value +"!="+ checkNumber);
+                    return true
+                } else {
+                    return false;
+                };
+            } else {
+                return false;
+            };
+    
+        }, true);
+        VLineTest = doku[position].VLine.reduce(function (testBool = true, VLineCell) {
+            if (testBool == true) {
+                if (doku[VLineCell].value != checkNumber) {
+                    //console.log(doku[VLineCell].value +"!="+ checkNumber);
+                    return true
+                } else {
+                    return false;
+                };
+            } else {
+                return false;
+            };
+    
+        }, true);
+    
+        if (gridTest && HLineTest && VLineTest) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
 
